3d75f5dcc775f19db2077708e70ed701f14e3a20
Eliyaho basson <basson131@gmail.com>
2023-07-23 20:42:18 +0300

    classes

---
+++ classes/classes.ts
@@ -0,0 +1,6 @@
+
+class Card {
+    constructor(public value: number, public color: string, public imgUrl: string, public isJoker:boolean) {
+    }
+}
+
---
+++ classes/dist/classes.js
@@ -0,0 +1,9 @@
+var Card = /** @class */ (function () {
+    function Card(value, color, imgUrl, isJoker) {
+        this.value = value;
+        this.color = color;
+        this.imgUrl = imgUrl;
+        this.isJoker = isJoker;
+    }
+    return Card;
+}());
