543d0f54eddd7c88b216ef24c39cdcf04c4691a7
Eliyaho basson <basson131@gmail.com>
2023-07-28 17:04:01 +0300

    board

--- trial/dist/index.js
+++ trial/dist/index.js
@@ -21,38 +21,148 @@ var player = [
     new Card(3, 'red', './imgs/3red.png', false),
     new Card(3, 'yellow', './imgs/3yellow.png', false),
 ];
+var i = 0;
+var Square = /** @class */ (function () {
+    function Square(isOccupied, imgUrl, value, color) {
+        this.isOccupied = isOccupied;
+        this.imgUrl = imgUrl;
+        this.value = value;
+        this.color = color;
+        this.id = i;
+        i++;
+    }
+    Square.prototype.setOccupiedAndCardProperties = function (card) {
+        try {
+            this.isOccupied = true;
+            this.imgUrl = card.imgUrl;
+            this.color = card.color;
+            this.value = card.value;
+        }
+        catch (error) {
+            console.error(error);
+        }
+    };
+    return Square;
+}());
 var playerElement = document.querySelector('#player');
 var html = "";
 player.forEach(function (card) {
-    playerElement.innerHTML += "<div class=\"card\" id = \"" + card.value + card.color + "\"style=\"background-image: url('" + card.imgUrl + "');\"></div>";
+    playerElement.innerHTML += "<div class=\"card\" id = \"" + card.value + card.color + "\" style=\"background-image: url('" + card.imgUrl + "');\"></div>";
 });
 var playerCards = document.querySelectorAll(".card");
+var board = [
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+];
+var boardElement = document.querySelector('#board');
+board.forEach(function (square) {
+    boardElement.innerHTML += "<div class=\"square\" id=\"" + square.id + "\"></div>";
+});
 var squares = document.querySelectorAll(".square");
+var currentCard;
+var indexOfCurrentCard;
+var currentCardElement;
 playerCards.forEach(function (element) {
     element.style.borderRadius = '7px';
-    element.addEventListener("mouseup", function () {
-    });
     element.addEventListener("mousedown", function () {
         element.style.position = 'fixed;';
-        document.addEventListener("mousemove", function (ev) {
-            element.style.position = "fixed;";
-            var x = ev.clientX;
-            var y = ev.clientY;
-            element.style.left = x - 50 + 'px';
-            element.style.top = y - 50 + 'px';
-            squares.forEach(function (square) {
-                square.addEventListener("click", function () {
-                    square.style.backgroundSize = 'cover';
-                    square.style.backgroundImage = element.style.backgroundImage;
-                    element.style.display = 'none';
-                    var indexOfCurrentCard = player.findIndex(function (card) {
-                        return "url(\"" + card.imgUrl + "\")" == "" + element.style.backgroundImage;
-                    });
-                    var newSeria = [];
-                    newSeria.push(player[indexOfCurrentCard]);
-                    console.log(newSeria);
-                });
+        indexOfCurrentCard = player.findIndex(function (card) { return "" + card.value + card.color === element.id; });
+        currentCard = player[indexOfCurrentCard];
+        if (currentCard !== undefined) {
+            document.addEventListener("mousemove", function (ev) {
+                element.style.position = "fixed;";
+                var x = ev.clientX;
+                var y = ev.clientY;
+                var xx = element.clientLeft;
+                element.style.left = x - 400 + "px";
+                element.style.top = y - 400 + "px";
             });
-        });
+        }
     });
+});
+function drawCard(square) {
+    try {
+        if (currentCard === undefined)
+            throw new Error('can not find current card');
+        square.style.backgroundSize = 'cover';
+        square.style.backgroundImage = "url(" + currentCard.imgUrl + ")";
+        currentCardElement = document.getElementById("" + currentCard.value + currentCard.color);
+        currentCardElement.style.display = 'none';
+        var currentSquare = board.find(function (sqr) { return sqr.id == square.id; });
+        if (!currentSquare)
+            throw new Error('can not find current square');
+        currentSquare.setOccupiedAndCardProperties(currentCard);
+        newSeria.push(currentCard);
+        console.log(newSeria);
+        currentCard = undefined;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+var newSeria = [];
+squares.forEach(function (square) {
+    square.addEventListener("click", function () {
+        drawCard(square);
+    });
+    square.addEventListener("mouseenter", function () {
+        square.style.border = '1px solid navy';
+        // square.style.background = 'rgb(0, 232, 232)'
+    });
+    square.addEventListener("mouseout", function () {
+        square.style.border = '';
+        // if(!square.style.backgroundImage){
+        //     square.style.background=''
+        // }
+    });
+    //     const rrr = currentCard.color
+    //     console.log(currentCard);
+    //     console.log(rrr);
+    //     square.style.backgroundSize = 'cover'
+    //     square.style.backgroundImage = rrr
+    //     // element.style.display = 'none'
+    //     // const indexOfCurrentCard = player.findIndex(card =>
+    //     //     `url("${card.imgUrl}")` == `${element.style.backgroundImage}`)
+    //     let newSeria: Card[] = []
+    //     // newSeria.push(player[indexOfCurrentCard])
+    //     // console.log(newSeria);
+    // })
 });
--- trial/index.ts
+++ trial/index.ts
@@ -17,50 +17,170 @@ const player: Card[] = [
     new Card(3, 'red', './imgs/3red.png', false),
     new Card(3, 'yellow', './imgs/3yellow.png', false),
 ]
+let i = 0
+
+class Square {
+    id: number
+    constructor(public isOccupied: boolean, public imgUrl?: string, public value?: number, public color?: string) {
+        this.id = i
+        i++
+    }
+
+    setOccupiedAndCardProperties(card: Card) {
+        try {
+            this.isOccupied = true
+            this.imgUrl = card.imgUrl
+            this.color = card.color
+            this.value = card.value
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
+
 
 const playerElement = document.querySelector('#player') as HTMLDivElement
 
 let html = ``
 
 player.forEach(card => {
-    playerElement.innerHTML += `<div class="card" id = "${card.value}${card.color}"style="background-image: url('${card.imgUrl}');"></div>`
+    playerElement.innerHTML += `<div class="card" id = "${card.value}${card.color}" style="background-image: url('${card.imgUrl}');"></div>`
+})
+const playerCards = document.querySelectorAll(".card") as NodeListOf<HTMLDivElement>
+
+const board: Square[] = [
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+    new Square(false),
+]
+const boardElement = document.querySelector('#board') as HTMLDivElement
+
+board.forEach(square => {
+    boardElement.innerHTML += `<div class="square" id="${square.id}"></div>`
+
 })
 
-const playerCards = document.querySelectorAll(".card") as NodeListOf<HTMLDivElement>
 const squares = document.querySelectorAll(".square") as NodeListOf<HTMLDivElement>
 
+let currentCard: Card | undefined
+let indexOfCurrentCard
+let currentCardElement: any
 
 playerCards.forEach(element => {
     element.style.borderRadius = '7px'
-    element.addEventListener("mouseup", function () {
-
-    })
     element.addEventListener("mousedown", function () {
         element.style.position = 'fixed;'
-        document.addEventListener("mousemove", function (ev) {
-            element.style.position = "fixed;"
-            let x = ev.clientX
-            let y = ev.clientY
-            element.style.left = x - 50 + 'px'
-            element.style.top = y - 50 + 'px'
+        indexOfCurrentCard = player.findIndex(card => `${card.value}${card.color}` === element.id)
+        currentCard = player[indexOfCurrentCard]
+        if (currentCard !== undefined) {
 
-            squares.forEach(square => {
-                square.addEventListener("click", function () {
-                    square.style.backgroundSize = 'cover'
-                    square.style.backgroundImage = element.style.backgroundImage
-                    element.style.display = 'none'
-                    const indexOfCurrentCard = player.findIndex(card =>
-                        `url("${card.imgUrl}")` == `${element.style.backgroundImage}`)
-                    let newSeria: Card[]=[]
-                    newSeria.push(player[indexOfCurrentCard])
-                    console.log(newSeria);
+            document.addEventListener("mousemove", function (ev) {
+                element.style.position = "fixed;"
+                let x = ev.clientX
+                let y = ev.clientY
+                let xx = element.clientLeft
+                element.style.left = `${x - 400}px`
+                element.style.top = `${y - 400}px`
 
-
-                })
 
             })
-        })
+        }
 
     })
+
+
+})
+
+function drawCard(square) {
+    try {
+        if (currentCard === undefined) throw new Error('can not find current card')
+
+        square.style.backgroundSize = 'cover'
+        square.style.backgroundImage = `url(${currentCard.imgUrl})`
+        currentCardElement = document.getElementById(`${currentCard.value}${currentCard.color}`)
+        currentCardElement.style.display = 'none'
+        const currentSquare = board.find(sqr => sqr.id == square.id)
+        if (!currentSquare) throw new Error('can not find current square')
+        currentSquare.setOccupiedAndCardProperties(currentCard)
+        newSeria.push(currentCard)
+        console.log(newSeria);
+        
+        currentCard=undefined
+    } catch (error) {
+        console.error(error)
+    }
+}
+const newSeria:Card[]=[]
+squares.forEach(square => {
+    square.addEventListener("click", function () {
+        drawCard(square)
+    })
+    square.addEventListener("mouseenter",function(){
+        square.style.border = '1px solid navy'
+        // square.style.background = 'rgb(0, 232, 232)'
+    })
+
+    square.addEventListener("mouseout",function(){
+        square.style.border = ''
+        // if(!square.style.backgroundImage){
+        //     square.style.background=''
+        // }
+
+    })
+
+
+    //     const rrr = currentCard.color
+    //     console.log(currentCard);
+    //     console.log(rrr);
+
+    //     square.style.backgroundSize = 'cover'
+    //     square.style.backgroundImage = rrr
+    //     // element.style.display = 'none'
+    //     // const indexOfCurrentCard = player.findIndex(card =>
+    //     //     `url("${card.imgUrl}")` == `${element.style.backgroundImage}`)
+    //     let newSeria: Card[] = []
+    //     // newSeria.push(player[indexOfCurrentCard])
+    //     // console.log(newSeria);
+
+
+    // })
 
 })
