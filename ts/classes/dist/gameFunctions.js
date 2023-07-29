fbe104377b66117dee26a236e92f2bad4f8ca51a
liran26 <liranav26@gmail.com>
2023-07-29 12:59:37 +0300

    more functions

--- ts/classes/dist/gameFunctions.js
+++ ts/classes/dist/gameFunctions.js
@@ -16,3 +16,99 @@ function computeScore(players) {
         console.error(error);
     }
 }
+function minimumSum(cardsToBoard) {
+    try {
+        if (cardsToBoard === undefined || !cardsToBoard)
+            throw new Error("No cards");
+        var sum_1 = 0;
+        cardsToBoard.forEach(function (card) {
+            sum_1 += card.value;
+        });
+        return sum_1 < 30 ? false : true;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function checkSerial(cards, card) {
+    try {
+        if (cards === undefined || !cards)
+            throw new Error("No cards");
+        if (cards.length < 3) {
+            alert("Minimum three cards");
+            return false;
+        }
+        if (card === undefined || !card) {
+            for (var i = 0; i < cards.length - 1; i++) {
+                if ((cards[i].value + 1) !== cards[i + 1].value)
+                    return false;
+            }
+        }
+        else {
+            if (cards[0].value - 1 !== card.value && cards[cards.length].value + 1 !== card.value)
+                return false;
+        }
+        return true;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function checkColors(cards, card) {
+    try {
+        if (cards === undefined || !cards)
+            throw new Error("No cards");
+        if (cards.length < 3) {
+            alert("Minimum three cards");
+            return false;
+        }
+        if (card === undefined || !card) {
+            for (var i = 0; i < cards.length - 1; i++) {
+                if (cards[i].color !== cards[i + 1].color)
+                    return false;
+            }
+        }
+        else {
+            if (cards[0].color !== card.color)
+                return false;
+        }
+        return true;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function checkserie(cards, card) {
+    try {
+        if (cards === undefined || !cards)
+            throw new Error("No cards");
+        if (cards.length < 3) {
+            alert("Minimum three cards");
+            return false;
+        }
+        if (card === undefined || !card) {
+            var colors_1 = [{ color: "red", set: true },
+                { color: "blue", set: true },
+                { color: "yellow", set: true },
+                { color: "black", set: true }];
+            cards.forEach(function (crd) {
+                var color = colors_1.find(function (c) { return c.color === crd.color; });
+                if (color === undefined)
+                    throw new Error("No such color");
+                if (!color.set)
+                    return false;
+                color.set = false;
+            });
+        }
+        else {
+            cards.forEach(function (c) {
+                if (c.color === card.color)
+                    return false;
+            });
+        }
+        return true;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
--- ts/classes/gameFunctions.ts
+++ ts/classes/gameFunctions.ts
@@ -1,4 +1,4 @@
-function computeScore(players: {player:Player, score:number}[]) {
+function computeScore(players: { player: Player, score: number }[]) {
     try {
         if (!players) throw new Error("Game element missing");
         players.forEach(player => {
@@ -6,7 +6,7 @@ function computeScore(players: {player:Player, score:number}[]) {
             player.player.cards.forEach((card) => {
                 sum += card.value;
             });
-            player.score -= sum; 
+            player.score -= sum;
         });
         //setToLocalStorage();
         //move to score window? href
@@ -15,3 +15,90 @@ function computeScore(players: {player:Player, score:number}[]) {
     }
 }
 
+function minimumSum(cardsToBoard: Card[]): boolean | undefined {
+    try {
+        if (cardsToBoard === undefined || !cardsToBoard) throw new Error("No cards");
+        let sum = 0;
+        cardsToBoard.forEach(card => {
+            sum += card.value;
+        })
+        return sum < 30 ? false : true;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function checkSerial(cards: Card[], card: Card): boolean | undefined {
+    try {
+        if (cards === undefined || !cards) throw new Error("No cards");
+        if (cards.length < 3) {
+            alert("Minimum three cards")
+            return false;
+        }
+        if (card === undefined || !card) {
+            for (let i = 0; i < cards.length - 1; i++) {
+                if ((cards[i].value + 1) !== cards[i + 1].value) return false;
+            }
+        }
+        else {
+            if (cards[0].value - 1 !== card.value && cards[cards.length].value + 1 !== card.value)
+                return false;
+        }
+        return true;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function checkColors(cards: Card[], card: Card): boolean | undefined {
+    try {
+        if (cards === undefined || !cards) throw new Error("No cards");
+        if (cards.length < 3) {
+            alert("Minimum three cards")
+            return false;
+        }
+        if (card === undefined || !card) {
+            for (let i = 0; i < cards.length - 1; i++) {
+                if (cards[i].color !== cards[i + 1].color) return false;
+            }
+        }
+        else {
+            if (cards[0].color !== card.color)
+                return false;
+        }
+        return true;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function checkserie(cards: Card[], card: Card): boolean | undefined {
+    try {
+        if (cards === undefined || !cards) throw new Error("No cards");
+        if (cards.length < 3) {
+            alert("Minimum three cards")
+            return false;
+        }
+        if (card === undefined || !card) {
+            const colors: { color: string, set: boolean }[] = [{ color: "red", set: true },
+            { color: "blue", set: true },
+            { color: "yellow", set: true },
+            { color: "black", set: true }]
+            cards.forEach(crd => {
+                const color = colors.find(c => c.color === crd.color)
+                if (color === undefined) throw new Error("No such color");
+                if (!color.set)
+                    return false;
+                color.set = false;
+            })
+        }
+        else {
+            cards.forEach(c => {
+                if (c.color === card.color) return false;
+            })
+        }
+        return true;
+    } catch (error) {
+        console.error(error)
+    }
+}
